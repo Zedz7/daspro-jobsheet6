a2ea960600e6d2b24a394d9571d29ba419759030
Zaimz <hafizahmadzaimkuntoro@gmail.com>
2024-10-04 18:27:19 +0700

    commit pertama

---
+++ Pemilihan2Percobaan106.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan106{
+    public static void main(String[] args) {
+        Scanner input06 = new Scanner (System.in);
+
+        int tahun;
+
+        System.out.print("Masukkan tahun: ");
+        tahun = input06.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0);
+                System.out.println("Tahun kabisat");
+        } else
+            System.out.println("Bukan tahun kabisat");
+    }
+    
+}
