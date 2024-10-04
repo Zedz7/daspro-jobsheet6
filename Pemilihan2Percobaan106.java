171adfeba5d4139abe3b1bcd93279b2bc4d06122
Zaimz <hafizahmadzaimkuntoro@gmail.com>
2024-10-04 19:27:13 +0700

    commit pertama

--- Pemilihan2Percobaan106.java
+++ Pemilihan2Percobaan106.java
@@ -10,12 +10,16 @@ public class Pemilihan2Percobaan106{
         tahun = input06.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0 || (tahun % 400) == 0 ) {
-                System.out.println("Tahun kabisat");
+            if ((tahun % 100) == 0) {
+                if ((tahun % 400) ==0) {
+                    System.out.println("Tahun kabisat");
+                } else {
+                    System.out.println("Bukan tahun kabisat");
+                }
             } else {
-                System.out.println("Bukan tahun kabisat");
+                System.out.println("Tahun kabisat");
             }
-        }  else {
+        } else {
             System.out.println("Bukan tahun kabisat");
         }
        
